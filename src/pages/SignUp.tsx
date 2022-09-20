29329f5ab71dd7e06fb409fd5d97db936ca6d76d
Gentrit Uka <gentrit-uka@live.com>
2022-09-20 23:31:48 +0200

    updated

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,41 +1,50 @@
 import { User } from "../App";
+import "./SignUp.css";
 
 type Props = {
-    signIn: (user: User) => void;
-}
+  signIn: (user: User) => void;
+};
 
 export function SignUp({ signIn }: Props) {
-    return (
-        <>
-            <h1>Create a new account</h1>
-            <form onSubmit={(event) => {
-                event.preventDefault()
-                const newUser = {
-                    username: event.target.username.value,
-                    email: event.target.email.value,
-                    password: event.target.password.value
-                }
+  return (
+      <div className="signup">
+        <h1>Create a new account</h1>
+        <form
+            onSubmit={(event) => {
+            event.preventDefault();
+            const newUser = {
+                username: event.target.username.value,
+                email: event.target.email.value,
+                password: event.target.password.value,
+            };
 
-                fetch('http://localhost:4000/signup', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }, 
-                    body: JSON.stringify(newUser)
-                }).then(r => r.json())
-                .then(data => {
-                    if(data.error){
-                        alert(data.error)
-                    } else {
-                        signIn(data)
-                    }
-                })
-            }}>
-                <input type="text" name="username" placeholder="username..." required/>
-                <input type="text" name="email" placeholder="email..." required/>
-                <input type="password" name="password" placeholder="password..." required/>
-                <button>Submit</button>
-            </form>
-        </>
-    )
+            fetch("http://localhost:4000/signup", {
+                method: "POST",
+                headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify(newUser),
+            })
+                .then((r) => r.json())
+                .then((data) => {
+                if (data.error) {
+                    alert(data.error);
+                } else {
+                    signIn(data);
+                }
+                });
+            }}
+        >
+            <input type="text" name="username" placeholder="username..." required />
+            <input type="text" name="email" placeholder="email..." required />
+            <input
+            type="password"
+            name="password"
+            placeholder="password..."
+            required
+            />
+            <button>Submit</button>
+        </form>
+      </div>
+  );
 }
