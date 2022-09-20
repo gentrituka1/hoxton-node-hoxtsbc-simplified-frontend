3a8a80ba2b6186ecb16abdabc7655b2ef8fc3cb9
Gentrit Uka <gentrit-uka@live.com>
2022-09-20 21:20:48 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import { useEffect, useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import { SignUp } from "./pages/Signup";
 
 export type Transaction = {
-  id: number
-  reason: string
-  amount: number
-  userId: number
-}
+  id: number;
+  reason: string;
+  amount: number;
+  userId: number;
+};
 
 export type User = {
-  id: number
-  username: string
-  email: string
-  password: string
-  transactions: Transaction[]
-}
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  transactions: Transaction[];
+};
 
 function App() {
-  const [user, setUser] = useState<User | null>(null)
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
 
+  function signIn(user: User) {
+    setCurrentUser(user);
+  }
+
+  function signOut() {
+    setCurrentUser(null);
+  }
 
   return (
     <div className="App">
@@ -27,10 +37,30 @@ function App() {
         <h1>Welcome</h1>
       </header>
       <Routes>
-        <Route index element={<Home />} />
+        <Route
+          index
+          element={
+            currentUser !== null ? (
+              <div>
+                <h1>Welcome, {currentUser.username}</h1>
+                <button
+                  onClick={() => {
+                    signOut();
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
